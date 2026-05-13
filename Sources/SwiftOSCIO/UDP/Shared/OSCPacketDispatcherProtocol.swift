bbceb8200071d82209de89eb7c84513bfe208d6b
Steffan Andrews <stef@orchetect.com>
2026-05-13 16:37:58 -0700

    Renamed file

--- Sources/SwiftOSCIO/UDP/Shared/OSCPacketDispatcherProtocol.swift
+++ Sources/SwiftOSCIO/UDP/Shared/OSCPacketDispatcherProtocol.swift
@@ -1,5 +1,5 @@
 //
-//  OSCHandlerProtocol.swift
+//  OSCPacketDispatcherProtocol.swift
 //  SwiftOSC I/O: Cocoa • https://github.com/orchetect/swift-osc-io-cocoa
 //  © 2026 Steffan Andrews • Licensed under MIT License
 //
